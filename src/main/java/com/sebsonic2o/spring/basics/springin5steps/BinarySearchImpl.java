96ea7e9a59d9052666c6ef7a38829918ae40c3dd
Sebastien Cheraux <scheraux@gmail.com>
2020-12-15 16:16:54 -0800

    initialize maven project and add tightly coupled dummy binary search

---
+++ src/main/java/com/sebsonic2o/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,10 @@
+package com.sebsonic2o.spring.basics.springin5steps;
+
+public class BinarySearchImpl {
+
+	public int binarySearch(int[] numbers, int numberToSearchFor) {
+		// Sorting
+		// Searching
+		return 3;
+	}
+}
