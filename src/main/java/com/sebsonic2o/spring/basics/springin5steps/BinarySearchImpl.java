3cdfdc992671eb71aaf7fed83161aadbf0e40c20
Sebastien Cheraux <scheraux@gmail.com>
2020-12-16 16:32:54 -0800

    autowire by qualifier instead of name

--- src/main/java/com/sebsonic2o/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/sebsonic2o/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,17 +1,19 @@
 package com.sebsonic2o.spring.basics.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 
 	@Autowired
-	private SortAlgorithm quickSortAlgorithm;
+	@Qualifier("quick")
+	private SortAlgorithm sortAlgorithm;
 
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 		// Sorting
-		int[] sortedNumbers = quickSortAlgorithm.sort(numbers);
+		int[] sortedNumbers = sortAlgorithm.sort(numbers);
 		// Searching
 		return 0;
 	}
--- src/main/java/com/sebsonic2o/spring/basics/springin5steps/BubbleSortAlgorithm.java
+++ src/main/java/com/sebsonic2o/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.sebsonic2o.spring.basics.springin5steps;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("bubble")
 public class BubbleSortAlgorithm implements SortAlgorithm {
 
 	@Override
