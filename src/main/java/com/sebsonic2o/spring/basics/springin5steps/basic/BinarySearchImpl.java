635ba0855c772bd7d322b148474c1ee49c425d2d
Sebastien Cheraux <scheraux@gmail.com>
2020-12-17 12:57:56 -0800

    leverage post construct and pre destroy

--- src/main/java/com/sebsonic2o/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/sebsonic2o/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,5 +1,10 @@
 package com.sebsonic2o.spring.basics.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -7,8 +12,11 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
+
+	private Logger logger =
+			LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	@Qualifier("quick")
@@ -19,5 +27,15 @@ public class BinarySearchImpl {
 		int[] sortedNumbers = sortAlgorithm.sort(numbers);
 		// Searching
 		return 0;
+	}
+
+	@PostConstruct
+	public void doPostConstructAction() {
+		logger.info("postConstruct");
+	}
+
+	@PreDestroy
+	public void doPreDestroyAction() {
+		logger.info("preDestroy");
 	}
 }
